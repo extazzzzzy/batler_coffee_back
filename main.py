92c13f4922aaa37466ef636eb7b2412ee294f212
extazy <danyakovalugrasu@gmail.com>
2025-04-06 00:51:39 +0500

    new fix

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from dotenv import load_dotenv
 from app.router import create_router
 import uvicorn
 import asyncio
-from app.tgbot import start_bot  # Импорт функции запуска бота
 from app.dependencies import supabase
 import multiprocessing
 import os
@@ -22,29 +21,17 @@ async def health_check():
 def run_fastapi():
     uvicorn.run(app, host=os.getenv("DEPLOY_HOST"), port=int(os.getenv("DEPLOY_PORT")))
 
-async def run_telegram_bot():
-    await start_bot()
-
-def run_bot_wrapper():
-    # Обертка для запуска бота в отдельном процессе
-    asyncio.run(run_telegram_bot())
-
 if __name__ == "__main__":
-    # Создаем процессы для сервера и бота
+    # Создаем процессы для сервера
     fastapi_process = multiprocessing.Process(target=run_fastapi)
-    #bot_process = multiprocessing.Process(target=run_bot_wrapper)
     fastapi_process.start()
-    #bot_process.start()
 
     try:
         # Ожидаем завершения процессов (фактически будет работать бесконечно)
         fastapi_process.join()
-        #bot_process.join()
     except KeyboardInterrupt:
         # Корректная обработка Ctrl+C
         print("\nОстановка сервера...")
         fastapi_process.terminate()
-        #bot_process.terminate()
         fastapi_process.join()
-        #bot_process.join()
         print("Сервер и бот остановлены")
