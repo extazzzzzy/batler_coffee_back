0db54b0e1da4b923cdcd6c2f0a790c142385f32d
extazy <danyakovalugrasu@gmail.com>
2025-04-19 18:47:25 +0500

    cors politics

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from app.router import create_router
 import uvicorn
 from app.dependencies import supabase
 import os
+from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv()
 
@@ -11,6 +12,15 @@ app = FastAPI()
 
 # Подключаем роутер
 app.include_router(create_router(supabase))
+
+allowed_origins = os.getenv("ALLOWED_ORIGINS", "").split(",")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=allowed_origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def health_check():
